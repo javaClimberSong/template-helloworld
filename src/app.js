06846ce7136c2330ec969865b980b80519263fb8
liuzhou1 <670457194@qq.com>
2024-09-27 14:49:49 +0800

    feat: 优化生成视频后直传oss

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@
 const express = require("express");
 const { exec } = require("child_process");
 const FormData = require("form-data");
-const https = require("https");
-const OSS = require("ali-oss");
-const path = require("path");
 const fs = require("fs");
-
+const axios = require("axios");
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 app.use(express.json());
 
 // 允许跨域访问
@@ -22,37 +19,7 @@ app.use((req, res, next) => {
 // 定义接口，用于执行本地脚本
 app.get("/downLoad", (req, res) => {
   console.log("收到请求", req.query);
-  const { model, props } = req.query;
-  const videoName = "298534.jpg";
-  const data = fs.createReadStream("./out/" + videoName);
-  // console.log("同步读取: " + data);
-  const config = {
-    accessId: "LTAI5tMxvwWhMh4oK8HTLtqZ",
-    policy:
-      "eyJleHBpcmF0aW9uIjoiMjAyNC0wOS0yNlQxMTo0NzoxMS43NzRaIiwiY29uZGl0aW9ucyI6W1siY29udGVudC1sZW5ndGgtcmFuZ2UiLDAsMTA0ODU3NjAwMF0sWyJzdGFydHMtd2l0aCIsIiRrZXkiLCJ2aWRlby9yZW5kZXIvMjAyNC0wOS0yNi8zNjIzMjI5ODcxNTk3NjQ5OTIiXV19",
-    signature: "z/G220j0fb0BKqSIbal5DJS+pkI=",
-    host: "https://open2other.oss-cn-hangzhou.aliyuncs.com/",
-    expire: "1727351231",
-    callback: null,
-    url: "https://open2other.oss-cn-hangzhou.aliyuncs.com/video/render/2024-09-26/362322987159764992/",
-    key: "video/render/2024-09-26/362322987159764992/",
-    shopName: null
-  };
-  const formData = new FormData();
-  formData.append("key", `${config.key}${videoName}`);
-  formData.append("policy", config.policy);
-  formData.append("OSSAccessKeyId", config.accessId);
-  formData.append("success_action_status", "200");
-  formData.append("signature", config.signature);
-  formData.append("file", data);
-  console.log("formData", formData);
-  fetch(config.host, {
-    method: "POST",
-    headers: { "content-type": "multipart/form-data" },
-    body: formData
-  }).then((res) => {
-    console.log("文件已上传");
-  });
+  res.send(`get请求弃用:`);
 
   // exec(
   //   `npx remotion render ${model} out/${videoName}.mp4 --props ${JSON.stringify(props)}`,
@@ -90,8 +57,38 @@ app.post("/downLoad", (req, res) => {
         return res.status(500).send(`Script Error: ${stderr}`);
       }
 
-      // 脚本执行成功，返回输出
-      res.send(`脚本输出成功: ${stdout}`);
+      // 发送 POST 请求
+      axios({
+        url: "https://360-dev.byering.com/ai/dialog/config/getOssGeneratePolicyVideo",
+        method: "get"
+      }).then((results) => {
+        const { entity: config } = results.data;
+        console.log("config", config);
+        const uploadVideoName = videoName + ".mp4";
+        if (config) {
+          const data = fs.createReadStream("./out/" + uploadVideoName);
+          const formData = new FormData();
+          formData.append("key", `${config.key}${uploadVideoName}`);
+          formData.append("policy", config.policy);
+          formData.append("OSSAccessKeyId", config.accessId);
+          formData.append("Signature", config.signature);
+          formData.append("file", data);
+          axios({
+            method: "post",
+            url: config.host,
+            data: formData
+          })
+            .then(() => {
+              res.send("文件已上传成功，url:" + config.url + uploadVideoName);
+              console.log("文件已上传url", config.url + uploadVideoName);
+            })
+            .catch((err) => {
+              res.send("文件上传失败");
+              console.log("文件上传失败", err);
+            });
+        }
+        console.log("文件key", res.data);
+      });
     }
   );
 });
